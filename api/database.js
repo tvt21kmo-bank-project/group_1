8ed00190aeed2d7886796753e1042bf2bf6efd10
hezixx <80600972+hezixx@users.noreply.github.com>
2021-11-14 22:03:56 +0200

    Update database.js

--- api/database.js
+++ api/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
-	password:'Teletappi',
-	database:'kvgbank'
+	password:'',	//Muista oman tietokannan salasana ja tietokannan nimi!!
+	database:''	
 });
 connection.connect(function(error){
 	if(!!error) {

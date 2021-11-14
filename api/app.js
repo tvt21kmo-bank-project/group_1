f8c3b27b4d39e3a27ebc1dd8aa9e3ab3b873f21a
hezixx <80600972+hezixx@users.noreply.github.com>
2021-11-14 21:59:07 +0200

    CRUD-operaatiot
    Tässäpä ois tietokannalle CRUD operaatiot tehtynä

---
+++ api/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var asiakasRouter = require('./routes/asiakas');
+var korttiRouter = require('./routes/kortti');
+var tiliRouter = require('./routes/tili');
+var tapahtumaRouter = require('./routes/tapahtumat');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/asiakas', asiakasRouter);
+app.use('/kortti',korttiRouter);
+app.use('/tili', tiliRouter);
+app.use('/tapahtumat',tapahtumaRouter);
+
+module.exports = app;
